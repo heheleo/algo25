71757751f2a6194810d719b9736f93cd536ff376
Leo <leowasntfound@gmail.com>
2025-02-07 16:44:43 +1100

    fix: change font of body

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Bricolage Grotesque",
+        body: "Source Sans Pro",
         code: "JetBrains Mono",
       },
       colors: {
